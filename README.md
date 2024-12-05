40356ad72ef349a6f1489f4d877f2fa985b9402c
Steve-xmh <39523898+Steve-xmh@users.noreply.github.com>
2024-12-05 17:49:35 +0800

    init

---
+++ README.md
@@ -0,0 +1,25 @@
+# SteveXMH's TnTRF Mod
+
+A simple mod for PC version of Taiko no Tatsujin: Rhythm Festival.
+
+一个简易的 PC 版本的 太鼓之达人：咚咚雷音祭 的模组。
+
+## Features / 功能
+
+- Allow to treat one side hit as a big hit, which will behave as the arcade version. (Will be disabled when in online mode to avoid being detected as cheating)
+- Faster boot scene to quickly get into game.
+
+- 允许将单侧击打强制识别成大打，和街机行为一致（在线上模式下会被自动禁用以免被视为作弊）
+- 将启动画面速度加快，可以更快地进入游戏
+
+## Installation / 安装方式
+
+Install [MelonLoader](https://github.com/LavaGang/MelonLoader) into your game, then download this mod from the [release page](https://github.com/Steve-xmh/TnTRFMod/releases/latest) and put it in the `Mods` folder under the game directory and start the game.
+
+为你的游戏安装好 [MelonLoader](https://github.com/LavaGang/MelonLoader)，然后从[发行页面](https://github.com/Steve-xmh/TnTRFMod/releases/latest)中下载本模组并放入游戏目录下的 `Mods` 文件夹后启动游戏即可。
+
+## Disclaimer / 免责声明
+
+This mod is not fully tested in multiplayer games. Even if it can disable some functions that are unfair to online game, it **CANNOT** ensure that they will not be considered as cheating tools. If you are worried about this, it is recommended to disable or delete this mod before playing online mode.
+
+本模组并未在多人游戏中进行完整测试，即便可以自主禁用部分有失多人公平性的功能但**无法确保**被视作使用作弊工具，如果担心这种情况则建议在游玩多人模式前禁用或删除本模组。
---
+++ TnTRFMod.cs
@@ -0,0 +1,113 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+using Il2CppScripts.OutGame.Boot;
+using MelonLoader;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using static Il2Cpp.TaikoCoreTypes;
+
+namespace TnTRFMod
+{
+    public class TnTRFMod: MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            base.OnInitializeMelon();
+            LoggerInstance.Msg("TnTRFMod has started!");
+        }
+
+        private static Rect addExpButton = new Rect(10, 10, 100, 50);
+        public static bool isOnlineMode = false;
+
+        private void DrawGUI()
+        {
+            // TODO
+        }
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+        }
+
+        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
+        {
+            base.OnSceneWasLoaded(buildIndex, sceneName);
+            // Il2Cpp.TaikoCorePlayer/SimpleInput
+            if (sceneName == "Boot")
+            {
+                //SceneManager.LoadScene("Title");
+            }
+            if (sceneName == "MyRoom")
+            {
+                MelonEvents.OnGUI.Subscribe(DrawGUI, 100);
+            }
+            else
+            {
+                MelonEvents.OnGUI.Unsubscribe(DrawGUI);
+            }
+        }
+    }
+
+    namespace SimpleInputPatch
+    {
+
+        [HarmonyPatch(typeof(EnsoInput), "GetLastInputForCore", new Type[] { typeof(int) })]
+        public class GetLastInputForCorePatch
+        {
+            static void Postfix(EnsoInput __instance, ref UserInputType __result, int player)
+            {
+                if (!TnTRFMod.isOnlineMode)
+                {
+                    switch (__result)
+                    {
+                        case UserInputType.Don_Weak:
+                        case UserInputType.Don_Pad:
+                            __result = UserInputType.Don_Strong;
+                            break;
+                        case UserInputType.Katsu_Weak:
+                        case UserInputType.Katsu_Pad:
+                            __result = UserInputType.Katsu_Strong;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+
+
+        [HarmonyPatch(typeof(EnsoPlayingParameter), "get_IsOnlineMode", new Type[] { })]
+        public class IsOnlineModePatch
+        {
+            static void Postfix(EnsoPlayingParameter __instance, bool __result)
+            {
+                if (TnTRFMod.isOnlineMode != __result)
+                {
+                    TnTRFMod.isOnlineMode = __result;
+                    if (__result)
+                    {
+                        MelonLogger.Msg($"识别到在线模式，强制大打已禁用");
+                        MelonLogger.Msg($"Detected online mode, force big hit patch has been disabled");
+                    }
+                    else
+                    {
+                        MelonLogger.Msg($"识别到非在线模式，强制大打已启用");
+                        MelonLogger.Msg($"Detected non-online mode, force big hit patch has been enabled");
+                    }
+                }
+            }
+        }
+
+
+        [HarmonyPatch(typeof(BootImage), "PlayAsync", new Type[] { typeof(float), typeof(bool) })]
+        public class ShortenBootImagePatch
+        {
+            static void Prefix(BootImage __instance, ref float duration, ref bool skippable)
+            {
+                duration = 0f;
+                skippable = true;
+                MelonLogger.Msg("已强制缩短启动画面时间并强制允许跳过");
+            }
+        }
+    }
+}
