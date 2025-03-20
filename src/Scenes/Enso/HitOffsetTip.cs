e4c838de6f358cbe61322645b66db21eddbe424e
Steve-xmh <39523898+Steve-xmh@users.noreply.github.com>
2025-03-20 16:53:47 +0800

    修正 MelonLoader 构建

--- src/Scenes/Enso/HitOffsetTip.cs
+++ src/Scenes/Enso/HitOffsetTip.cs
@@ -1,7 +1,12 @@
-using TMPro;
 using TnTRFMod.Patches;
 using TnTRFMod.Ui.Widgets;
 using UnityEngine;
+
+#if BEPINEX
+using TMPro;
+#elif MELONLOADER
+using Il2CppTMPro;
+#endif
 
 namespace TnTRFMod.Scenes.Enso;
 
